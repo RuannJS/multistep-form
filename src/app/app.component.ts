930bd24e2ca14eeb6268c782ec95bcbf1ddb9b8a
Ruann Fagundes <ruanndev@gmail.com>
2023-11-22 00:26:24 -0300

    Responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FormService } from './services/form.service';
+import { Step } from './models/step-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(public readonly service: FormService) {}
 
-  steps: string[] = ['1', '2', '3', '4'];
+  getScreenWidth!: number;
+
+  steps: Step[] = [
+    {
+      step: '1',
+      text: 'your info',
+    },
+    {
+      step: '2',
+      text: 'select plan',
+    },
+    {
+      step: '3',
+      text: 'add-ons',
+    },
+    {
+      step: '4',
+      text: 'summary',
+    },
+  ];
+
+  ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
 }
